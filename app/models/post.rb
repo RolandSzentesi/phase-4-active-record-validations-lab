0fbc43824fe52c4ad4d46b7731e0c686eff8f5c8
RolandSzentesi <rampageroli@gmail.com>
2023-08-13 13:30:24 -0500

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
 class Post < ApplicationRecord
+    validate :click_bait?
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+
+    CLICK_BAITY = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top \d/i,
+        /Guess/i
+    ]
+
+
+    def click_bait?
+        if CLICK_BAITY.none? { |t| t.match title }
+            errors.add(:title, "must be clickbait")
+        end
+    end
+
 end
